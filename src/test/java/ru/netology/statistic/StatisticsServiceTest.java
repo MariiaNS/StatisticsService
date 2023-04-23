f58d3075a60287963f44c4d386d2ba444f3b9a1c
Maria <smn0101@yandex.ru>
2023-04-23 16:44:19 +0300

    add JoCoCo and Test

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -17,4 +17,17 @@ public class StatisticsServiceTest {
 
         Assertions.assertEquals(expected, actual);
     }
+
+    @Test
+    void findMaxInMiddle() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 20, 8, 6, 11, 11, 12};
+        long expected = 20;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
 }
